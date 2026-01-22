5e0810946ee9289b8b2456a72d1c8ba130678a8c
Maycol <maycolperezbadilla@gmail.com>
2026-01-22 08:55:22 -0600

    changes semana 3

---
+++ src/model/Paciente.java
@@ -0,0 +1,47 @@
+package com.odontologia.model;
+
+public class Paciente extends Persona {
+
+    private int numeroExpediente;
+
+    public Paciente() {
+    }
+
+    // Constructor con parámetros
+    public Paciente(int id, String nombre, int telefono, int numeroExpediente) {
+        super(id, nombre, telefono);
+        this.numeroExpediente = numeroExpediente;
+    }
+
+    // Getters y Setters
+    public int getNumeroExpediente() {
+        return numeroExpediente;
+    }
+
+    public void setNumeroExpediente(int numeroExpediente) {
+        this.numeroExpediente = numeroExpediente;
+    }
+
+    // Métodos del diagrama
+    public void solicitarCita() {
+        System.out.println("Método solicitarCita no implementado");
+    }
+
+    public void solicitarReprogramacion() {
+        System.out.println("Método solicitarReprogramacion no implementado");
+    }
+
+    public void justificarAusencia() {
+        System.out.println("Método justificarAusencia no implementado");
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "numeroExpediente=" + numeroExpediente +
+                ", id=" + getId() +
+                ", nombre='" + getNombre() + '\'' +
+                ", telefono=" + getTelefono() +
+                '}';
+    }
+}
